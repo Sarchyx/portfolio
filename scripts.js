71366e408455573282eaf42d6920d2975d9bfe63
David Díaz <75103519+Sarchyx@users.noreply.github.com>
2025-11-19 10:19:33 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,22 +5,21 @@ let videos = [
 	{ src: './assets/merge-game.mp4', title: 'Merge Game' },
 	{ src: './assets/dealership.mp4', title: 'Car Dealership' },
 	{ src: './assets/customization.mp4', title: 'Car Customization' },
+    { src: './assets/inventory.mp4', title: 'Draggable Inventory' },
+    { src: './assets/cube-climbing.mp4', title: 'Climbing Game' },
 	{ src: './assets/first-person-mechs.mp4', title: 'FP Mechanics' },
-	{ src: './assets/cube-climbing.mp4', title: 'Climbing Game' },
-	{ src: './assets/inventory.mp4', title: 'Draggable Inventory' },
-	{ src: './assets/lazer-puzzle.mp4', title: 'Lazer Puzzle' },
+    { src: './assets/simon.mp4', title: 'Simon Minigame' },
 	{ src: './assets/spell-drawings.mp4', title: 'Spell Drawings' },
-	{ src: './assets/simon.mp4', title: 'Simon Minigame' },
+    { src: './assets/angle-calculator.mp4', title: 'Angle Calculator' },
 	{ src: './assets/blood-iron.mp4', title: 'Blood & Iron Core' },
 	{ src: './assets/terror-ai.mp4', title: 'Terror AI' },
 	{ src: './assets/menu.mp4', title: 'Menu UI Animations' },
+    { src: './assets/intro.mp4', title: 'Intro' },
 	{ src: './assets/xmas.mp4', title: 'Xmas Showcase' },
-	{ src: './assets/angle-calculator.mp4', title: 'Angle Calculator' },
-	{ src: './assets/intro.mp4', title: 'Intro' },
+    { src: './assets/placement.mp4', title: 'Placement System' },
 	{ src: './assets/ik.mp4', title: 'Inverse Kinematics' },
 	{ src: './assets/reward.mp4', title: 'Reward Animation' },
 	{ src: './assets/tornado.mp4', title: 'Trigonometry Tornado' },
-	{ src: './assets/placement.mp4', title: 'Placement System' }
 ];
 
 const videoListContainer = document.getElementById('video-list');
